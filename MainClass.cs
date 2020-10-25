1f448f30049d41708939ae19804fa278cbda46b0
kalyangoud145 <kalyangoud145@gmail.com>
2020-10-25 11:13:26 +0530

    Add project files.

---
+++ MainClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookTest
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook Problem");
+        }
+    }
+}
